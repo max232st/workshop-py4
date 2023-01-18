0daac71160a6af3dc956d36e395cca4eccccaa2f
max232st <max232st@gmail.com>
2023-01-18 18:19:45 +0300

    dz

--- task006_dz/task006_dz.py
+++ task006_dz/task006_dz.py
@@ -1,14 +1,6 @@
-# Вычислить число c заданной точностью d
-# Пример:
-# - при $d = 0.001, π = 3.141.$
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв""
 
-from math import pi
-count = 0
-d = (input("Введите число :\n"))
-for i in range(0, len(d)):
-    if d[i].isdigit() == False:
-        i += 1
-        for j in range(i, len(d)):
-            count += 1
-
-print(f'число Пи с заданной точностью {d} равно {round(pi, count)}')
+s = 'пра прабв огнр длорпабвовгв зщзыфшвщ вфыыабвгыоы абвывовдлыф"'.split()
+print(s)
+result = filter(lambda x: "а" not in x or "б" not in x or "в" not in x, s)
+print(*result)
